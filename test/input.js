876e353786b846a76137465ddbbd8ec2eddd64c4
Rob Loach <robloach@gmail.com>
2015-04-18 08:26:50 -0400

    style(tests): Fix the whitespace in the tests

--- test/input.js
+++ test/input.js
@@ -1,12 +1,12 @@
 var merge = require('merge');
 
 var array1 = {
-	salutation: 'Goodbye',
-	name: 'World!'
+  salutation: 'Goodbye',
+  name: 'World!'
 };
 var array2 = {
-	salutation: 'Hello',
-	seperator: ' '
+  salutation: 'Hello',
+  seperator: ' '
 };
 
 var output = merge(array1, array2);
